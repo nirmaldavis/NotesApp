0e7b255c127bb10c764bde53a23dbc2fc8deadd9
nirmaldavis <nirmal.davis@gmail.com>
2016-11-01 12:48:23 +0530

    Chapter 5 factory service
    Chapter 5 factory service sample

--- app.js
+++ app.js
@@ -11,20 +11,43 @@ notesApp.controller("MainCtrl", [function() {
     };
 }]);
 
-notesApp.controller("SubCtrl", ['$log', '$location',function($log, $location){
+notesApp.controller("SubCtrl", ['$log', 'ItemService', '$location',function($log, ItemService, $location){
     
     console.log("In SubCtrl");
     
     var self = this;
-    self.list = [
-        { id:1, label:"Item 0"},
-        { id:2, label:"Item 1"}
-    ];
+
+    
+    self.list = function() {
+
+        return ItemService.list();
+    };
+    
+    
     self.add = function() {
-        self.list.push({id:self.list.length + 1, label:"Item " +  self.list.length});
+        
+        ItemService.add({
+                id : self.list().length+1,
+                label : 'Item ' + (self.list().length + 1)
+            });
+        
         //Log this - added new item
         $log.log("Added new Item");
         $log.log($location.absUrl());
     };
 
+}]);
+
+notesApp.factory("ItemService", [function() {
+
+    var items = [{ id: 1, label: "Item 1"}, { id: 2, label: "Item 2"}];
+    
+    return {
+        list : function() {
+            return items;
+        },
+        add : function(item) {
+            items.push(item);   
+        }
+    };
 }]);
